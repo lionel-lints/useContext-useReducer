131f0be5f35db49747e95f0c792d86649bad68be
Lionel Lints <lionel.lints@gmail.com>
2021-01-29 08:45:58 -0800

    split dispatch and subscription

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import {useContext} from 'react';
 import {store, StateProvider} from './store.js';
 
-const Paragraph = () => {
-  const values = useContext(store);
-  let toggleAction = () => values.dispatch({ type: 'TOGGLE_BOOL', payload: null })
+const SubscribedComponent = () => {
+  const { state } = useContext(store);
+
   return (
-    <>
-      <p>{JSON.stringify(values)}</p>
+    <p>{JSON.stringify(state)}</p>
+  );
+};
+
+const DispatchCapableButton = () => {
+  const { dispatch } = useContext(store);
+
+  let toggleAction = () => dispatch({ type: 'TOGGLE_BOOL', payload: null })
+
+  return (
       <button onClick={toggleAction}>
         toggle state
       </button>
-    </>
   );
 };
 
@@ -19,9 +26,9 @@ function App() {
     <div className="App">
       {/* It is crucial you wrap any component in a StateProvider if you want to be able to dispatch actions */}
       <StateProvider>
-        <Paragraph />
+        <SubscribedComponent />
+        <DispatchCapableButton />
       </StateProvider>
-      <Paragraph />
     </div>
   );
 }
